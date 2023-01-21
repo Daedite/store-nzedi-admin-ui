91d5e70e96cb032d9b0bc725c985ca8a6cb7bfee
ChairmanT <taylor@dwebserver.net>
2023-01-21 20:33:16 +0200

    first test of the login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import {Button, Card} from "react-bootstrap";
+import React from 'react';
+import './component/App.css';
+import LoginPage from './component/login/login';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  return(
-      <Card >
-        <Button> Click me! </Button>
-      </Card>
-  )
+  return (
+    <div className="login d-flex justify-content-center ">
+      <div className="container">
+        <div className="card">
+          <div className="card-body">
+            <LoginPage />
+          </div>
+        </div>
+      </div>
+    </div>
+   
+  );
 }
 
 export default App;
