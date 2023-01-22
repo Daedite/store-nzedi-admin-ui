7d6a8aca6003add1e393f7848c66582a13876017
ChairmanT <taylor@dwebserver.net>
2023-01-22 22:02:52 +0200

    Comment my previous page and added the new one, now i need to css it to center and have a log in front

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="login d-flex justify-content-center ">
+    <div className="login d-flex justify-content-center">
       <div className="container">
         <div className="card">
           <div className="card-body">
